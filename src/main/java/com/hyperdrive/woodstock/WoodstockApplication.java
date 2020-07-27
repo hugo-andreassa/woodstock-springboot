5be76f7e3f43afe85543d63088e15231266df8ae
Hugo Andreassa <hugo.andreassa@gmail.com>
2020-07-27 13:20:33 -0300

    Projeto criado

---
+++ src/main/java/com/hyperdrive/woodstock/WoodstockApplication.java
@@ -0,0 +1,13 @@
+package com.hyperdrive.woodstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyperdrive/woodstock/WoodstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperdrive.woodstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
